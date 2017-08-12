ec9bdc04791738dceda8e7f47087a20a7567bf3b
Sabrina <skq501@york.ac.uk>
2017-08-12 18:53:07 +0100

    Update basicGame.js

--- games/basicGame.js
+++ games/basicGame.js
@@ -1,10 +1,21 @@
-$(“#playground”).playground({refreshRate: 60});
-  
-  //initialize the start button
-        $("#startbutton").click(function(){
-          $.playground().startGame(function(){
-            $("#welcomeScreen").fadeTo(1000,0,function(){$(this).remove();});
-          });
-        })
+// Global constants:
+var PLAYGROUND_WIDTH    = 700;
+var PLAYGROUND_HEIGHT    = 250;
+var REFRESH_RATE        = 30;
 
+
+//Initialise the game
+$(“#playground”).playground(height: PLAYGROUND_HEIGHT, 
+        width: PLAYGROUND_WIDTH, 
+        keyTracker: true});
+  
+//Set id of loading bar
 $().setLoadBar("loadingBar", 400);
+
+//initialize the start button
+$("#startbutton").click(function(){
+$.playground().startGame(function(){
+$("#welcomeScreen").remove();
+});
+})
+
