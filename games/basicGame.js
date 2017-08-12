fffacec9fe204d44aa51ce6f64b84020ee985dd9
Sabrina <skq501@york.ac.uk>
2017-08-12 18:44:59 +0100

    Update basicGame.js

--- games/basicGame.js
+++ games/basicGame.js
@@ -1,1 +1,10 @@
-$(“#basicGame”).playground({refreshRate: 60});
+$(“#playground”).playground({refreshRate: 60});
+  
+  //initialize the start button
+        $("#startbutton").click(function(){
+          $.playground().startGame(function(){
+            $("#welcomeScreen").fadeTo(1000,0,function(){$(this).remove();});
+          });
+        })
+
+$().setLoadBar("loadingBar", 400);
